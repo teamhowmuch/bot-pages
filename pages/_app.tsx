21a7af2d5ba07cf252b1038fbf4525608cf3c5fd
daanaerts <daanaerts@gmail.com>
2022-08-23 22:50:43 +0200

    add matomo 2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,6 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Script id="matomo-analytics" strategy="afterInteractive">
         {`
-          <script>
           var _paq = window._paq = window._paq || [];
           /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
           _paq.push(['trackPageView']);
@@ -19,7 +18,6 @@ function MyApp({ Component, pageProps }: AppProps) {
             var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
             g.async=true; g.src='//cdn.matomo.cloud/grobot.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
           })();
-        </script>
         `}
       </Script>
 
