a271a7619788828bf29d721435bbe75e5e38e0ea
daanaerts <daanaerts@gmail.com>
2022-10-03 10:00:26 +0200

    grobot img

--- lib/components/Navbar.tsx
+++ lib/components/Navbar.tsx
@@ -1,6 +1,7 @@
+import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./Button";
-import { Container } from "./Container";
+import avatar from "../../public/images/avatar.png";
 
 type Props = {};
 
@@ -8,11 +9,15 @@ export function Navbar(props: Props) {
   return (
     <div className="text-center bg-white py-1">
       <div className="flex px-3 items-center">
-        <div className="w-36"></div>
-        <div className="flex-grow">
-          <h1 className="text-xl">🤖 grobot</h1>
+        <div className="w-36 flex-0"></div>
+
+        <div className="flex flex-1 items-center justify-center gap-2">
+          <div style={{ width: 35 }}>
+            <Image src={avatar} height={35} width={35} alt={`avatar-grobot`} />
+          </div>
+          <h1 className="font-bold">grobot</h1>
         </div>
-        <div className="w-36 flex justify-end">
+        <div className="w-36 flex flex-0 justify-end">
           <Link
             href="https://landbot.pro/v3/H-1344766-1F4J8SWG83O073XV/index.html"
             target="_blank"
